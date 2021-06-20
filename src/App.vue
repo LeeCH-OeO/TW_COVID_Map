eb60d0c39525c50df55946613b29fb305797aed7
LeeCH-OeO <fairprice81000@gmail.com>
2021-06-20 21:19:04 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -134,237 +134,237 @@ export default {
       if (this.DateData[4] >= 100) {
         L.marker([22.992237581041017, 120.18504985784304], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 臺南市新增確診數: ${this.DateData[4]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 臺南市新增確診數: ${this.DateData[4]}</strong>`);
       } else if (this.DateData[4] >= 50) {
         L.marker([22.992237581041017, 120.18504985784304], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 臺南市新增確診數: ${this.DateData[4]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 臺南市新增確診數: ${this.DateData[4]}</strong>`);
       } else {
         L.marker([22.992237581041017, 120.18504985784304], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 臺南市新增確診數: ${this.DateData[4]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 臺南市新增確診數: ${this.DateData[4]}</strong>`);
       }
       if (this.DateData[5] >= 100) {
         L.marker([22.620838509364138, 120.31191206948549], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 高雄市新增確診數: ${this.DateData[5]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 高雄市新增確診數: ${this.DateData[5]}</strong>`);
       } else if (this.DateData[5] >= 50) {
         L.marker([22.620838509364138, 120.31191206948549], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 高雄市新增確診數: ${this.DateData[5]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 高雄市新增確診數: ${this.DateData[5]}</strong>`);
       } else {
         L.marker([22.620838509364138, 120.31191206948549], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 高雄市新增確診數: ${this.DateData[5]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 高雄市新增確診數: ${this.DateData[5]}</strong>`);
       }
 
       if (this.DateData[6] >= 100) {
         L.marker([25.13175594879303, 121.74445523314445], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 基隆市新增確診數: ${this.DateData[6]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 基隆市新增確診數: ${this.DateData[6]}</strong>`);
       } else if (this.DateData[6] >= 50) {
         L.marker([25.13175594879303, 121.74445523314445], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 基隆市新增確診數: ${this.DateData[6]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 基隆市新增確診數: ${this.DateData[6]}</strong>`);
       } else {
         L.marker([25.13175594879303, 121.74445523314445], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 基隆市新增確診數: ${this.DateData[6]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 基隆市新增確診數: ${this.DateData[6]}</strong>`);
       }
       if (this.DateData[7] >= 100) {
         L.marker([24.80677334722868, 120.96878362748409], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 新竹市新增確診數: ${this.DateData[7]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 新竹市新增確診數: ${this.DateData[7]}</strong>`);
       } else if (this.DateData[7] >= 50) {
         L.marker([24.80677334722868, 120.96878362748409], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 新竹市新增確診數: ${this.DateData[7]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 新竹市新增確診數: ${this.DateData[7]}</strong>`);
       } else {
         L.marker([24.80677334722868, 120.96878362748409], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 新竹市新增確診數: ${this.DateData[7]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 新竹市新增確診數: ${this.DateData[7]}</strong>`);
       }
       if (this.DateData[8] >= 100) {
         L.marker([23.48134011673181, 120.45358827657498], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 嘉義市新增確診數: ${this.DateData[8]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 嘉義市新增確診數: ${this.DateData[8]}</strong>`);
       } else if (this.DateData[8] >= 50) {
         L.marker([23.48134011673181, 120.45358827657498], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 嘉義市新增確診數: ${this.DateData[8]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 嘉義市新增確診數: ${this.DateData[8]}</strong>`);
       } else {
         L.marker([23.48134011673181, 120.45358827657498], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 嘉義市新增確診數: ${this.DateData[8]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 嘉義市新增確診數: ${this.DateData[8]}</strong>`);
       }
       if (this.DateData[9] >= 100) {
         L.marker([24.826893789998294, 121.01288760476697], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 新竹縣新增確診數: ${this.DateData[9]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 新竹縣新增確診數: ${this.DateData[9]}</strong>`);
       } else if (this.DateData[9] >= 50) {
         L.marker([24.826893789998294, 121.01288760476697], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 新竹縣新增確診數: ${this.DateData[9]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 新竹縣新增確診數: ${this.DateData[9]}</strong>`);
       } else {
         L.marker([24.826893789998294, 121.01288760476697], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 新竹縣新增確診數: ${this.DateData[9]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 新竹縣新增確診數: ${this.DateData[9]}</strong>`);
       }
       if (this.DateData[10] >= 100) {
         L.marker([24.564869651259347, 120.82072294036129], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 苗栗縣新增確診數: ${this.DateData[10]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 苗栗縣新增確診數: ${this.DateData[10]}</strong>`);
       } else if (this.DateData[10] >= 50) {
         L.marker([24.564869651259347, 120.82072294036129], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 苗栗縣新增確診數: ${this.DateData[10]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 苗栗縣新增確診數: ${this.DateData[10]}</strong>`);
       } else {
         L.marker([24.564869651259347, 120.82072294036129], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 苗栗縣新增確診數: ${this.DateData[10]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 苗栗縣新增確診數: ${this.DateData[10]}</strong>`);
       }
       if (this.DateData[11] >= 100) {
         L.marker([24.075431171010795, 120.54448991128714], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 彰化縣新增確診數: ${this.DateData[11]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 彰化縣新增確診數: ${this.DateData[11]}</strong>`);
       } else if (this.DateData[11] >= 50) {
         L.marker([24.075431171010795, 120.54448991128714], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 彰化縣新增確診數: ${this.DateData[11]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 彰化縣新增確診數: ${this.DateData[11]}</strong>`);
       } else {
         L.marker([24.075431171010795, 120.54448991128714], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 彰化縣新增確診數: ${this.DateData[11]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 彰化縣新增確診數: ${this.DateData[11]}</strong>`);
       }
       if (this.DateData[12] >= 100) {
         L.marker([23.902747845242303, 120.69049836917395], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 南投縣新增確診數: ${this.DateData[12]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 南投縣新增確診數: ${this.DateData[12]}</strong>`);
       } else if (this.DateData[12] >= 50) {
         L.marker([23.902747845242303, 120.69049836917395], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 南投縣新增確診數: ${this.DateData[12]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 南投縣新增確診數: ${this.DateData[12]}</strong>`);
       } else {
         L.marker([23.902747845242303, 120.69049836917395], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 南投縣新增確診數: ${this.DateData[12]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 南投縣新增確診數: ${this.DateData[12]}</strong>`);
       }
       if (this.DateData[13] >= 100) {
         L.marker([23.699210536514624, 120.5263276980079], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 雲林縣新增確診數: ${this.DateData[13]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 雲林縣新增確診數: ${this.DateData[13]}</strong>`);
       } else if (this.DateData[13] >= 50) {
         L.marker([23.699210536514624, 120.5263276980079], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 雲林縣新增確診數: ${this.DateData[13]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 雲林縣新增確診數: ${this.DateData[13]}</strong>`);
       } else {
         L.marker([23.699210536514624, 120.5263276980079], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 雲林縣新增確診數: ${this.DateData[13]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 雲林縣新增確診數: ${this.DateData[13]}</strong>`);
       }
       if (this.DateData[14] >= 100) {
         L.marker([23.458866668851602, 120.2927905403322], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 嘉義縣新增確診數: ${this.DateData[14]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 嘉義縣新增確診數: ${this.DateData[14]}</strong>`);
       } else if (this.DateData[14] >= 50) {
         L.marker([23.458866668851602, 120.2927905403322], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 嘉義縣新增確診數: ${this.DateData[14]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 嘉義縣新增確診數: ${this.DateData[14]}</strong>`);
       } else {
         L.marker([23.458866668851602, 120.2927905403322], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 嘉義縣新增確診數: ${this.DateData[14]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 嘉義縣新增確診數: ${this.DateData[14]}</strong>`);
       }
       if (this.DateData[15] >= 100) {
         L.marker([22.683047293281277, 120.48788945206347], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 屏東縣新增確診數: ${this.DateData[15]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 屏東縣新增確診數: ${this.DateData[15]}</strong>`);
       } else if (this.DateData[15] >= 50) {
         L.marker([22.683047293281277, 120.48788945206347], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 屏東縣新增確診數: ${this.DateData[15]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 屏東縣新增確診數: ${this.DateData[15]}</strong>`);
       } else {
         L.marker([22.683047293281277, 120.48788945206347], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 屏東縣新增確診數: ${this.DateData[15]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 屏東縣新增確診數: ${this.DateData[15]}</strong>`);
       }
       if (this.DateData[16] >= 100) {
         L.marker([24.730695549273918, 121.7630588669248], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 宜蘭縣新增確診數: ${this.DateData[16]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 宜蘭縣新增確診數: ${this.DateData[16]}</strong>`);
       } else if (this.DateData[16] >= 50) {
         L.marker([24.730695549273918, 121.7630588669248], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 宜蘭縣新增確診數: ${this.DateData[16]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 宜蘭縣新增確診數: ${this.DateData[16]}</strong>`);
       } else {
         L.marker([24.730695549273918, 121.7630588669248], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 宜蘭縣新增確診數: ${this.DateData[16]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 宜蘭縣新增確診數: ${this.DateData[16]}</strong>`);
       }
       if (this.DateData[17] >= 100) {
         L.marker([23.99134066698878, 121.61981142709575], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 花蓮縣新增確診數: ${this.DateData[17]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 花蓮縣新增確診數: ${this.DateData[17]}</strong>`);
       } else if (this.DateData[17] >= 50) {
         L.marker([23.99134066698878, 121.61981142709575], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 花蓮縣新增確診數: ${this.DateData[17]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 花蓮縣新增確診數: ${this.DateData[17]}</strong>`);
       } else {
         L.marker([23.99134066698878, 121.61981142709575], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 花蓮縣新增確診數: ${this.DateData[17]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 花蓮縣新增確診數: ${this.DateData[17]}</strong>`);
       }
       if (this.DateData[18] >= 100) {
         L.marker([22.755603812562413, 121.15034661512443], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 臺東縣新增確診數: ${this.DateData[18]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 臺東縣新增確診數: ${this.DateData[18]}</strong>`);
       } else if (this.DateData[18] >= 50) {
         L.marker([22.755603812562413, 121.15034661512443], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 臺東縣新增確診數: ${this.DateData[18]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 臺東縣新增確診數: ${this.DateData[18]}</strong>`);
       } else {
         L.marker([22.755603812562413, 121.15034661512443], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 臺東縣新增確診數: ${this.DateData[18]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 臺東縣新增確診數: ${this.DateData[18]}</strong>`);
       }
       if (this.DateData[19] >= 100) {
         L.marker([23.570038519024905, 119.5663892040662], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 澎湖縣新增確診數: ${this.DateData[19]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 澎湖縣新增確診數: ${this.DateData[19]}</strong>`);
       } else if (this.DateData[19] >= 50) {
         L.marker([23.570038519024905, 119.5663892040662], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 澎湖縣新增確診數: ${this.DateData[19]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 澎湖縣新增確診數: ${this.DateData[19]}</strong>`);
       } else {
         L.marker([23.570038519024905, 119.5663892040662], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 澎湖縣新增確診數: ${this.DateData[19]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 澎湖縣新增確診數: ${this.DateData[19]}</strong>`);
       }
       if (this.DateData[20] >= 100) {
         L.marker([24.436936393471804, 118.31871402531816], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 金門縣新增確診數: ${this.DateData[20]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 金門縣新增確診數: ${this.DateData[20]}</strong>`);
       } else if (this.DateData[20] >= 50) {
         L.marker([24.436936393471804, 118.31871402531816], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 金門縣新增確診數: ${this.DateData[20]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 金門縣新增確診數: ${this.DateData[20]}</strong>`);
       } else {
         L.marker([24.436936393471804, 118.31871402531816], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 金門縣新增確診數: ${this.DateData[20]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 金門縣新增確診數: ${this.DateData[20]}</strong>`);
       }
       if (this.DateData[21] >= 100) {
         L.marker([26.15789157351567, 119.9518981403661], { icon: redIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 連江縣新增確診數: ${this.DateData[21]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 連江縣新增確診數: ${this.DateData[21]}</strong>`);
       } else if (this.DateData[21] >= 50) {
         L.marker([26.15789157351567, 119.9518981403661], { icon: yelloIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 連江縣新增確診數: ${this.DateData[21]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 連江縣新增確診數: ${this.DateData[21]}</strong>`);
       } else {
         L.marker([26.15789157351567, 119.9518981403661], { icon: greenIcon })
           .addTo(osmMap)
-          .bindPopup(`<strong>${this.selected_date} 連江縣新增確診數: ${this.DateData[21]}}</strong>`);
+          .bindPopup(`<strong>${this.selected_date} 連江縣新增確診數: ${this.DateData[21]}</strong>`);
       }
       L.marker([33.26906266433787, 131.3673314216881]).addTo(osmMap)
         .bindPopup('<a href="https://bit.ly/3gGFTnW">ゆふいんちょう</a>');
